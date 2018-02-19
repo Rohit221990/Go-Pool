944d733ed89d9983e1436baa86e7299accf03865
jangidrohit <jangid.rohit@xcdify.com>
2018-02-19 08:58:13 +0530

    changes for heroku

--- modules/users/controllers/usersController.js
+++ modules/users/controllers/usersController.js
@@ -49,7 +49,7 @@ module.exports.saveUsers = function (req, res, done) {
         updateTime:user.updatedAt,
         username:user.username
       }
-      res.send({ status: 'success', msg: 'users saved successfully', data: user});
+      res.send({ status: 'success', msg: 'users saved successfully', userData: user});
     }
   ], function (err) {
     if (err) {
