8e349faf218df483fc2d07798077ce66f4fcd4ef
jangidrohit <jangid.rohit@xcdify.com>
2018-03-05 00:11:11 +0530

    PUT api changes

--- modules/users/routes/usersRoute.js
+++ modules/users/routes/usersRoute.js
@@ -10,7 +10,7 @@ module.exports = function (app, router) {
   router.get('/users', users.getUsers);
   router.get('/users/:id', users.getUsers);
   router.post('/users', users.saveUsers);
-  router.put('/users/:id', users.saveUsers);
+  router.put('/users', users.saveUsers);
    router.delete('/users/:id', users.deleteUsers);
   // load the BOM router in the app
   app.use('/api/v1', router);
